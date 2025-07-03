6da7c5c8a6d91608692c188e4e0614b0e6797216
zcy <3041105031@qq.com>
2025-07-03 20:29:49 +0800

    房源增加栋数和房间号

--- app/Http/Controllers/Api/ShopsController.php
+++ app/Http/Controllers/Api/ShopsController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ShopRequest;
+use App\Http\Resources\Shop\ShopInfoResource;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ShopsController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    /**
+     * 新增
+     * @param ShopRequest $request
+     * @param Shop $shop
+     * @return ShopInfoResource
+     */
+    public function store(ShopRequest $request, Shop $shop): ShopInfoResource
+    {
+        $user = $request->user();
+        $shop->fill($request->all());
+        $shop->user()->associate($user);
+        $shop->save();
+        return new ShopInfoResource($shop);
+    }
 }
