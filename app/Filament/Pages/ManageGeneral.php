422ce3b5126506c6e451e3158439f07c3b84120c
zcy <3041105031@qq.com>
2025-07-15 22:50:54 +0800

    增加基础设置

--- app/Filament/Pages/ManageGeneral.php
+++ app/Filament/Pages/ManageGeneral.php
@@ -5,8 +5,10 @@ namespace App\Filament\Pages;
 use App\Settings\GeneralSettings;
 use Filament\Forms;
 use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
+use FilamentTiptapEditor\TiptapEditor;
 
 class ManageGeneral extends SettingsPage
 {
@@ -27,22 +29,42 @@ class ManageGeneral extends SettingsPage
                         Tabs\Tab::make('Tab 1')
                             ->label('会员设置')
                             ->schema([
-                                // ...
+                                Textarea::make('user_level_benefit')
+                                    ->autosize()
+                                    ->label('会员权益'),
+                                Textarea::make('user_level_rule')
+                                    ->autosize()
+                                    ->label('会员规则'),
+                                Textarea::make('user_level_other')
+                                    ->autosize()
+                                    ->label('会员其他说明'),
                             ]),
                         Tabs\Tab::make('Tab 2')
                             ->label('加入我们')
                             ->schema([
-                                // ...
+                                TiptapEditor::make('join_us')
+                                    ->profile('default')
+                                    ->label('')
+                                    ->maxContentWidth('5xl')
+                                    ->required()
                             ]),
                         Tabs\Tab::make('Tab 3')
                             ->label('公司介绍')
                             ->schema([
-                                // ...
+                                TiptapEditor::make('company_intro')
+                                    ->profile('default')
+                                    ->label('')
+                                    ->maxContentWidth('5xl')
+                                    ->required()
                             ]),
                         Tabs\Tab::make('Tab 4')
                             ->label('隐私协议')
                             ->schema([
-                                // ...
+                                TiptapEditor::make('privacy_policy')
+                                    ->profile('default')
+                                    ->label('')
+                                    ->maxContentWidth('5xl')
+                                    ->required()
                             ]),
                     ])
             ]);
