abf25d293d8eaa6024f2231aeddfbe757f4c818e
zcy <3041105031@qq.com>
2025-07-15 22:25:44 +0800

    优化代码

--- resources/views/tables/columns/facility-type.blade.php
+++ resources/views/tables/columns/facility-type.blade.php
@@ -1,14 +1,16 @@
 <div class="px-4 py-3">
     @foreach($getState() as $item)
-        @if($item === 0)
-            <x-filament::badge>
-                房源
-            </x-filament::badge>
-        @endif
-        @if($item === 1)
-            <x-filament::badge>
-                商铺
-            </x-filament::badge>
-        @endif
+        <div class="flex flex-row space-x-2">
+            @if($item === 0)
+                <x-filament::badge>
+                    房源
+                </x-filament::badge>
+            @endif
+            @if($item === 1)
+                <x-filament::badge>
+                    商铺
+                </x-filament::badge>
+            @endif
+        </div>
     @endforeach
 </div>
