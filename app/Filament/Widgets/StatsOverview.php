a2ae15e77ddf71c6013556cb81490de68e6acb62
zcy <3041105031@qq.com>
2025-07-15 23:10:06 +0800

    增加基础设置

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\House;
+use App\Models\Shop;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -9,11 +11,15 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $houseCount = House::query()->count();
+        $todayHouseCount = House::query()->whereDate('created_at', today())->count();
+        $shopCount = Shop::query()->count();
+        $todayShopCount = Shop::query()->whereDate('created_at', today())->count();
         return [
-            Stat::make('房源数量', 0),
-            Stat::make('今日新增房源', 0),
-            Stat::make('商铺数量', 0),
-            Stat::make('今日新增商铺', 0),
+            Stat::make('房源数量', $houseCount),
+            Stat::make('今日新增房源', $todayHouseCount),
+            Stat::make('商铺数量', $shopCount),
+            Stat::make('今日新增商铺', $todayShopCount),
         ];
     }
 }
