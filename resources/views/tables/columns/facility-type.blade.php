41e871cb0b408e934e0f0920ab4fa7d121896ac6
zcy <3041105031@qq.com>
2025-07-15 22:15:01 +0800

    优化代码

--- resources/views/tables/columns/facility-type.blade.php
+++ resources/views/tables/columns/facility-type.blade.php
@@ -1,14 +1,14 @@
 <div class="px-4 py-3">
     {{ $getState() }}
 
-    @if(in_array(0, $getState()))
-        <x-filament::badge>
-            房源
-        </x-filament::badge>
-    @endif
-    @if(in_array(1, $getState()))
-        <x-filament::badge>
-            商铺
-        </x-filament::badge>
-    @endif
+    {{--    @if(in_array(0, $getState()))--}}
+    {{--        <x-filament::badge>--}}
+    {{--            房源--}}
+    {{--        </x-filament::badge>--}}
+    {{--    @endif--}}
+    {{--    @if(in_array(1, $getState()))--}}
+    {{--        <x-filament::badge>--}}
+    {{--            商铺--}}
+    {{--        </x-filament::badge>--}}
+    {{--    @endif--}}
 </div>
