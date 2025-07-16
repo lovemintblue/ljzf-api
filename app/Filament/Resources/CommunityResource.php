f7dd9b9bbf979539cd7e06f858c2b713a99c9177
zcy <3041105031@qq.com>
2025-07-16 21:50:20 +0800

    提交代码

--- app/Filament/Resources/CommunityResource.php
+++ app/Filament/Resources/CommunityResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CommunityResource\Pages;
 use App\Filament\Resources\CommunityResource\RelationManagers;
+use App\Models\BusinessDistrict;
 use App\Models\Community;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -19,7 +20,7 @@ class CommunityResource extends Resource
     protected static ?string $model = Community::class;
 
     protected static ?string $navigationIcon = 'heroicon-m-squares-2x2';
-    
+
     protected static ?string $navigationLabel = '小区';
 
     protected static ?string $label = '小区';
@@ -37,12 +38,31 @@ class CommunityResource extends Resource
                     ->required()
                     ->columnSpanFull()
                     ->maxLength(255),
+                Forms\Components\TextInput::make('province')
+                    ->label('省份')
+                    ->required()
+                    ->columnSpanFull()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name')
+                    ->label('城市')
+                    ->required()
+                    ->columnSpanFull()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name')
+                    ->label('区县')
+                    ->required()
+                    ->columnSpanFull()
+                    ->maxLength(255),
                 Forms\Components\Textarea::make('address')
                     ->label('详细地址')
                     ->autosize()
                     ->columnSpanFull()
                     ->required()
                     ->maxLength(255),
+                Forms\Components\Select::make('business_district_id')
+                    ->label('关联商圈')
+                    ->columnSpanFull()
+                    ->options(BusinessDistrict::query()->pluck('name', 'id'))
             ]);
     }
 
@@ -54,8 +74,21 @@ class CommunityResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('businessDistrict.name')
+                    ->label('商圈')
+                    ->badge()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('name')
                     ->label('名称')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('province')
+                    ->label('省份')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('city')
+                    ->label('城市')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('district')
+                    ->label('区县')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('address')
                     ->label('详细地址')
