9e8c0109761263c6b916a7f4aabaf132f8eea926
zcy <3041105031@qq.com>
2025-07-16 21:22:23 +0800

    小区增加省份城市区县

---
+++ database/migrations/2025_07_16_200051_add_district_to_communities_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('communities', static function (Blueprint $table) {
+            $table->string('province')->comment('省份')->nullable();
+            $table->string('city')->comment('城市')->nullable();
+            $table->string('district')->comment('区县')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('communities', static function (Blueprint $table) {
+            $table->dropColumn('province', 'city', 'district');
+        });
+    }
+};
