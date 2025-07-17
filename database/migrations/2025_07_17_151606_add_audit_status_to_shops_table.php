4039aa649abae066327e20fc6e8475ee6e70664d
zcy <3041105031@qq.com>
2025-07-17 15:26:34 +0800

    提交代码

---
+++ database/migrations/2025_07_17_151606_add_audit_status_to_shops_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('shops', static function (Blueprint $table) {
+            $table->tinyInteger('audit_status')->comment('审核状态 0审核中 1审核通过 2审核驳回')->default(0);
+            $table->string('refuse_reason')->comment('驳回原因')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('shops', static function (Blueprint $table) {
+            $table->dropColumn('audit_status');
+        });
+    }
+};
