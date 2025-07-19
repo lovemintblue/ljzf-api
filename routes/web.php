148f69172acefca9be4cb8cef8f8b21a6e46be83
zcy <3041105031@qq.com>
2025-07-19 22:19:48 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,8 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
     $postStr = json_encode([
         'specify' => '156360702',
         'queryType' => 13,
-        'start' => 0,
-        'count' => 50,
+        'start' => 10,
+        'count' => 100,
         'dataTypes' => '120201',
         'show' => 2
     ]);
