3135fc27e4b713a9334de774efd45657f1305b88
zcy <3041105031@qq.com>
2025-07-19 21:26:37 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
         'specify' => '156110000', // 注意：specify 需为字符串格式
         'queryType' => '12',
         'start' => 0,
-        'count' => 100,
+        'count' => 1,
         'dataTypes' => '120201',
     ]);
 
