89f172725f810e2efc863388d7de604762a7bc04
zcy <3041105031@qq.com>
2025-07-19 21:33:16 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
 
 
     $response = Http::asJson()->get($api, [
-        'postStr' => $postData,
+        'postStr' => $postStr,
         'type' => 'query',
         'tk' => '5731ae54a2b2ab10697a929c5b6b8e11'
     ]);
