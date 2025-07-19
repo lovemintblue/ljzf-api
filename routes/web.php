ee726c102239f6ca813b85194ddfe9ee85d60710
zcy <3041105031@qq.com>
2025-07-19 22:15:24 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
         'type' => 'query',
         'tk' => '5731ae54a2b2ab10697a929c5b6b8e11'
     ]);
+    dd($response->json());
 
     $list = [];
     foreach ($response->json()['pois'] as $item) {
