7f501b78bccf568c852089c422815eb23578ee99
zcy <3041105031@qq.com>
2025-07-19 22:53:15 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,7 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
         'tk' => '5731ae54a2b2ab10697a929c5b6b8e11'
     ]);
     $totalCount = $response->json()['count'] ?? 0; // 总数据条数
+    dd($totalCount);
     // 若总条数≤100，支持分页；否则仅获取前100条
     $maxStart = max(0, $totalCount - $pageSize);
     do {
