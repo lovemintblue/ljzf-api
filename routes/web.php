e09ac461539f28c9ce54d2a529242f3cdc52a3a0
zcy <3041105031@qq.com>
2025-07-19 22:14:47 +0800

    完善代码

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('test2', static function (\Illuminate\Http\Request $request) {
         'specify' => '156360702',
         'queryType' => 13,
         'start' => 0,
-        'count' => 100,
+        'count' => 200,
         'dataTypes' => '120201',
         'show' => 2
     ]);
