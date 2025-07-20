2c418aea697ec00d2c170046a8990f6ae456f0b7
zcy <3041105031@qq.com>
2025-07-21 00:14:13 +0800

    优化后台

--- app/Filament/Resources/HouseResource.php
+++ app/Filament/Resources/HouseResource.php
@@ -109,6 +109,7 @@ class HouseResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('id')
                     ->label('ID'),
