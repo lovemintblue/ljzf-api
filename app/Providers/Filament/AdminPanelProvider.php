e07c690581d0b428801abf8c17a5f1fbeaf42d57
zcy <3041105031@qq.com>
2025-07-23 02:26:35 +0800

    优化后台

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -84,6 +84,7 @@ class AdminPanelProvider extends PanelProvider
                 '数据',
                 '用户',
                 '房源',
+                '商铺',
                 '系统'
             ])
             ->sidebarWidth('15rem')
