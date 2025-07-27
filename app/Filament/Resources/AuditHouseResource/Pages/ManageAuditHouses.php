1fe1e1e1593c9f0698feab1a79b38469689271fb
zcy <3041105031@qq.com>
2025-07-28 01:30:54 +0800

    完善代码

--- app/Filament/Resources/AuditHouseResource/Pages/ManageAuditHouses.php
+++ app/Filament/Resources/AuditHouseResource/Pages/ManageAuditHouses.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Resources\AuditHouseResource\Pages;
 
 use App\Filament\Resources\AuditHouseResource;
+use Asmit\ResizedColumn\HasResizableColumn;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageAuditHouses extends ManageRecords
 {
+    use HasResizableColumn;
+
     protected static string $resource = AuditHouseResource::class;
 
     protected function getHeaderActions(): array
