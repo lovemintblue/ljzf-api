59510422b70fc26d8dafab0af6d38f4ca34b2502
zcy <3041105031@qq.com>
2025-07-28 01:19:42 +0800

    完善代码

--- resources/views/tables/columns/business-district.blade.php
+++ resources/views/tables/columns/business-district.blade.php
@@ -1,11 +1,12 @@
 @php use App\Models\BusinessDistrict; @endphp
 <div class="px-3 py-3">
-    @foreach(BusinessDistrict::query()->whereIn('id', $getState())->get() as $item)
-        <div>
-            <x-filament::badge>
-                {{$item->name ?? ''}}
-            </x-filament::badge>
-        </div>
-    @endforeach
-
+    @if(!empty($getState()))
+        @foreach(BusinessDistrict::query()->whereIn('id', $getState())->get() as $item)
+            <div>
+                <x-filament::badge>
+                    {{$item->name ?? ''}}
+                </x-filament::badge>
+            </div>
+        @endforeach
+    @endif
 </div>
