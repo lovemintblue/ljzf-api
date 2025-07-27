1e82f42a63ef591a86259a732f1878ccc1de44e4
zcy <3041105031@qq.com>
2025-07-28 01:22:53 +0800

    完善代码

--- resources/views/tables/columns/facility-type.blade.php
+++ resources/views/tables/columns/facility-type.blade.php
@@ -1,6 +1,6 @@
 <div class="px-4 py-3">
     @foreach($getState() as $item)
-        <div class="flex">
+        <div class="flex flex-row">
             @if((int)$item === 0)
                 <div>
                     <x-filament::badge>
@@ -9,7 +9,7 @@
                 </div>
             @endif
             @if((int)$item === 1)
-                <div>
+                <div style="margin-left: 2px">
                     <x-filament::badge>
                         商铺
                     </x-filament::badge>
