fba0125aee8381efbd0606f33e361b6fdb6a51bc
zcy <3041105031@qq.com>
2025-07-31 22:52:48 +0800

    优化代码

---
+++ database/migrations/2025_07_31_224945_add_backup_contact_to_houses_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('houses', static function (Blueprint $table) {
+            $table->string('backup_contact_name')->comment('备用联系人')->nullable();
+            $table->string('backup_contact_phone')->comment('备用联系方式')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('houses', static function (Blueprint $table) {
+            $table->dropColumn('backup_contact_name', 'backup_contact_phone');
+        });
+    }
+};
