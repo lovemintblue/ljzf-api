6819ecc356d63a2c175b7e197e9739458cda01ae
zcy <3041105031@qq.com>
2025-08-02 00:51:55 +0800

    完善代码

--- app/Http/Resources/Community/CommunityInfoResource.php
+++ app/Http/Resources/Community/CommunityInfoResource.php
@@ -33,7 +33,7 @@ class CommunityInfoResource extends JsonResource
     public function toArray(Request $request): array
     {
         $businessDistrict = [];
-        if (!empty($this->business_district_ids)) {
+        if (count($this->business_district_ids) > 0) {
             $businessDistrict = BusinessDistrict::query()->whereIn('id', $this->business_district_ids)->pluck('name')->toArray();
         }
 
