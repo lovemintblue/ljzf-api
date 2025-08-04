1fd6582701de171d9fc0e5c107633371aaebd573
zcy <3041105031@qq.com>
2025-08-04 23:57:54 +0800

    用户增加到期时间

---
+++ database/migrations/2025_08_04_225418_add_expire_at_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->timestamp('expired_at')->comment('到期时间')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->dropColumn('expired_at');
+        });
+    }
+};
