78d645f40498e4ee67197476d4e51586527175b0
bohelan <3041105031@qq.com>
2025-08-14 15:44:42 +0800

    提交测试

--- app/Observers/CommunityObserver.php
+++ app/Observers/CommunityObserver.php
@@ -4,16 +4,24 @@ namespace App\Observers;
 
 use App\Models\Community;
 use App\Services\MapService;
+use Illuminate\Http\Client\ConnectionException;
 
 class CommunityObserver
 {
     /**
-     * Handle the Community "saving" event.
+     * saving
+     * @param Community $community
+     * @return void
+     * @throws ConnectionException
      */
     public function saving(Community $community): void
     {
-        if (!empty($community->address)) {
+        if ($community->address && $community->isDirty('address')) {
             $address = (new MapService())->geoCoder($community->address);
+            if (count($address) > 0) {
+                $community->longitude = $address['location']['lng'];
+                $community->latitude = $address['location']['lat'];
+            }
         }
     }
 }
