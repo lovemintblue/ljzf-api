72a9242e6da6a941ef53d263017a62e6052c8b54
bohelan <3041105031@qq.com>
2025-08-16 16:57:13 +0800

    同步数据

--- config/database.php
+++ config/database.php
@@ -111,14 +111,6 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-
-        'qiniu' => [
-            'driver' => 'qiniu',
-            'access_key' => env('QINIU_ACCESS_KEY', 'xxxxxxxxxxxxxxxx'),
-            'secret_key' => env('QINIU_SECRET_KEY', 'xxxxxxxxxxxxxxxx'),
-            'bucket' => env('QINIU_BUCKET', 'test'),
-            'domain' => env('QINIU_DOMAIN', 'xxx.clouddn.com'), // or host: https://xxxx.clouddn.com
-        ],
     ],
 
     /*
--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -59,7 +59,13 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
+        'qiniu' => [
+            'driver' => 'qiniu',
+            'access_key' => env('QINIU_ACCESS_KEY', 'xxxxxxxxxxxxxxxx'),
+            'secret_key' => env('QINIU_SECRET_KEY', 'xxxxxxxxxxxxxxxx'),
+            'bucket' => env('QINIU_BUCKET', 'test'),
+            'domain' => env('QINIU_DOMAIN', 'xxx.clouddn.com'), // or host: https://xxxx.clouddn.com
+        ],
     ],
 
     /*
